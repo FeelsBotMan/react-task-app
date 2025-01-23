c7cd860459ee8d100c69fd71d0494fb5c64d5ca6
FeelsBotMan <sjb0229@gmail.com>
2025-01-23 21:05:05 +0900

    deploy

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAgIOKikm05T37kkQsjI4sgUH7VM0x6-yU",
+    authDomain: "react-task-app-63f78.firebaseapp.com",
+    projectId: "react-task-app-63f78",
+    storageBucket: "react-task-app-63f78.firebasestorage.app",
+    messagingSenderId: "858574007854",
+    appId: "1:858574007854:web:4c6e5a2ae2bb814f42b9bf"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
